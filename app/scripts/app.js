0e809ac1217a763b79d8aa4b5b9537e1c3201404
ceseale3 <ceseale3@gmail.com>
2015-01-11 23:56:20 -0500

    set up node and angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name iotfiuApp
+ * @description
+ * # iotfiuApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('iotfiuApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
